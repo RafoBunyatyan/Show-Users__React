5a4c4f5ee9dc09363c3a9568731bfe00b344b655
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-06-02 22:12:16 +0400

    add class User

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,13 @@
 import classes from './User.module.css';
 
+class User {
+	render() {
+		return <li className={classes.user}>{props.name}</li>
+	}
+}
+
 const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+	return <li className={classes.user}>{props.name}</li>;
 };
 
 export default User;
